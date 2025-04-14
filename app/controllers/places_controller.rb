4c2f48c697f08f40b3e9ca22267bc05d28d4f29a
Mike Dalton <michaelcdalton@gmail.com>
2025-04-13 17:51:13 -0700

    Filter places by tags

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,13 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+    @tag_counts = Place.all.flat_map(&:tags).tally
+    @all_tags = @tag_counts.keys.sort_by { |tag| -@tag_counts[tag] }
+    
+    if params[:tag].present?
+      @places = @places.select { |place| place.tags.include?(params[:tag]) }
+      @selected_tag = params[:tag]
+    end
   end
 
   def show
