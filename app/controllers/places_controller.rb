784e436b2f96db400e9090210cf36238a791ed66
Mike Dalton <michaelcdalton@gmail.com>
2025-04-13 18:01:05 -0700

    Display 10 tags at first. The user can ask for more to be shown.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,7 @@ class PlacesController < ApplicationController
     @places = Place.all
     @tag_counts = Place.all.flat_map(&:tags).tally
     @all_tags = @tag_counts.keys.sort_by { |tag| -@tag_counts[tag] }
+    @display_tags = ActiveModel::Type::Boolean.new.cast(params[:show_all_tags]) ? @all_tags : @all_tags.first(10)
     
     if params[:tag].present?
       @places = @places.select { |place| place.tags.include?(params[:tag]) }
