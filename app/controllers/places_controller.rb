07be831d9dfa9671240cd15b4160fa6560d12c7f
Mike Dalton <michaelcdalton@gmail.com>
2025-04-14 00:48:40 -0700

    Filter by walking time

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,6 +9,20 @@ class PlacesController < ApplicationController
       @places = @places.joins(:tags).where(tags: { name: tag_name })
       @selected_tag_name = tag_name
     end
+
+    walking_time = params[:walking_time]
+    if walking_time.present?
+      max_seconds = case walking_time
+                   when '10'
+                     10 * 60
+                   when '20'
+                     20 * 60
+                   when '30'
+                     30 * 60
+                   end
+      @places = @places.where('walking_time_in_seconds <= ?', max_seconds) if max_seconds
+      @selected_walking_time = walking_time
+    end
   end
 
   def show
--- app/models/place.rb
+++ app/models/place.rb
@@ -4,7 +4,17 @@ class Place < ApplicationRecord
 
   validates_presence_of :name, :slug
 
+  after_save_commit :calculate_time_between_venue_and_place
+
   def google_maps_address
     address.present? ? address : "#{name},Philadelphia,PA"
+  end
+
+  private
+
+  def calculate_time_between_venue_and_place
+    if name_changed? || address_changed?
+      CalculateTimeBetweenVenueAndPlaceJob.perform_later(self.id)
+    end
   end
 end
