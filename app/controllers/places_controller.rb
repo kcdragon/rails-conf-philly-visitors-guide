8015ac2d5ea7b579c1e2e29261d5c50f68f40b02
Mike Dalton <michaelcdalton@gmail.com>
2025-04-23 20:17:46 -0400

    Fix formatting

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
     @places = Place.all.order(:name)
     @tags = Tag.where("places_count > ?", 0).order(places_count: :desc)
     @display_tags = ActiveModel::Type::Boolean.new.cast(params[:show_all_tags]) ? @tags : @tags.first(10)
-    
+
     tag_name = params[:tag_name]
     if tag_name.present?
       @places = @places.joins(:tags).where(tags: { name: tag_name })
@@ -12,15 +12,16 @@ class PlacesController < ApplicationController
 
     walking_time = params[:walking_time]
     if walking_time.present?
-      max_seconds = case walking_time
-                   when '10'
-                     10 * 60
-                   when '20'
-                     20 * 60
-                   when '30'
-                     30 * 60
-                   end
-      @places = @places.where('walking_time_in_seconds <= ?', max_seconds) if max_seconds
+      max_seconds =
+        case walking_time
+        when "10"
+          10 * 60
+        when "20"
+          20 * 60
+        when "30"
+          30 * 60
+        end
+      @places = @places.where("walking_time_in_seconds <= ?", max_seconds) if max_seconds
       @selected_walking_time = walking_time
     end
   end
